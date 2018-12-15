a32282bccea20a8e33170329c5e01326042fba14
Ubuntu <ubuntu@ip-172-31-20-16.us-gov-west-1.compute.internal>
2018-12-15 20:32:55 +0000

    reference iris2 defconfig and commit tag

--- make_var_som_mx6_debian.sh
+++ make_var_som_mx6_debian.sh
@@ -42,8 +42,8 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/uvdl/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="imx_4.9.88_2.0.0_ga-iris2-R0"
-readonly G_LINUX_KERNEL_REV="a2cc75c7c42772a1147557ff6018af11fc37ae44"
-readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_var_defconfig'
+readonly G_LINUX_KERNEL_REV="e5603ba1b440bfb416942b6d1772ce156b9f8596"
+readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_iris2_defconfig'
 readonly G_LINUX_DTB='imx6q-var-dart.dtb'
 
 ## uboot
