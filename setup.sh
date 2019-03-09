2edc5f1cf87b536fb3b0f7ac01ae801879754816
Vacaliuc, Bogdan <vacaliucb@ornl.gov>
2019-03-09 08:40:03 -0500

    fix serial number to 0 pad, matching general.Utility.serial_number; add lightdm to disabled services

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SERVICES="ModemManager"
-sn1=`cat /sys/fsl_otp/HW_OCOTP_CFG0 | cut -c3-`
-sn2=`cat /sys/fsl_otp/HW_OCOTP_CFG1 | cut -c3-`
-echo "IMX6${sn1}${sn2}" > /etc/hostname
+SERVICES="ModemManager lightdm"
+sn1=`cat /sys/fsl_otp/HW_OCOTP_CFG0`
+sn2=`cat /sys/fsl_otp/HW_OCOTP_CFG1`
+printf 'IMX6%0.8x%0.8x\n' $sn1 $sn2 > /etc/hostname
 cat /etc/hostname
 passwd
 for s in $SERVICES ; do
