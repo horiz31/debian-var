79adb6f2bb322b0b6299406f7ab9f32b9e14c522
Vacaliuc, Bogdan <vacaliucb@ornl.gov>
2019-05-24 18:27:16 -0400

    lately, the image has been coming up with this service enabled...

--- postinstall.sh
+++ postinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SERVICES="ModemManager lightdm"
+SERVICES="ModemManager lightdm hostapd"
 sn1=`cat /sys/fsl_otp/HW_OCOTP_CFG0`
 sn2=`cat /sys/fsl_otp/HW_OCOTP_CFG1`
 printf 'IMX6%0.8x%0.8x\n' $sn1 $sn2 > /etc/hostname
