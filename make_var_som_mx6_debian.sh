120681b4a8f0d941ad51e588acbec29fd444b4f5
Vacaliuc, Bogdan <vacaliucb@ornl.gov>
2019-06-08 14:27:10 -0400

    enable abb5zes3 and as6200 devices

--- make_var_som_mx6_debian.sh
+++ make_var_som_mx6_debian.sh
@@ -43,7 +43,7 @@ readonly G_VARISCITE_PATH="${DEF_BUILDENV}/variscite"
 readonly G_LINUX_KERNEL_SRC_DIR="${DEF_SRC_DIR}/kernel"
 readonly G_LINUX_KERNEL_GIT="https://github.com/uvdl/linux-imx.git"
 readonly G_LINUX_KERNEL_BRANCH="feature/iris2"
-readonly G_LINUX_KERNEL_REV="8be2b9f63a54d7d89f17c983e6ca5f55daacd166"
+readonly G_LINUX_KERNEL_REV="5d092d213d9464ddd5fe9657d42548d59d658c14"
 readonly G_LINUX_KERNEL_DEF_CONFIG='imx_v7_iris2_defconfig'
 readonly G_LINUX_DTB='imx6q-var-dart.dtb imx6q-iris2-R0.dtb imx6q-iris2-R1.dtb'
 
