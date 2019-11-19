e32eafbd245715cd5975834b5f5c00df9090ee07
Bogdan Vacaliuc <vacaliucb@ornl.gov>
2019-11-19 10:11:10 -0500

    add apt-daily*, NM and variscite-bluetooth to list of disabled services; add command-line override on number of WiFi antennas configured

--- postinstall.sh
+++ postinstall.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-SERVICES="ModemManager lightdm hostapd"
+SERVICES="ModemManager lightdm hostapd variscite-bluetooth apt-daily.timer apt-daily apt-daily-upgrade.timer apt-daily-upgrade NetworkManager-wait-online"
+TWOG=${1:-1}    # number of 2.4 GHz antennas (default: 1)
+FIVG=${2:-0}    # number of 5.8 GHz antennas (default: 0)
+# setup hostname to match serial number read from fuses
 sn1=`cat /sys/fsl_otp/HW_OCOTP_CFG0`
 sn2=`cat /sys/fsl_otp/HW_OCOTP_CFG1`
 printf 'IMX6%0.8x%0.8x\n' $sn1 $sn2 > /etc/hostname
 cat /etc/hostname
+# change root password
 passwd
+# disable services that slow down boot and interfere with operations
 for s in $SERVICES ; do
-	for c in stop disable ; do systemctl ${c} ${s} ; done
+	do systemctl disable --now ${s} ; done
 done
+# configure wifi chip and load initial firmware
 ( cd /usr/sbin/wlconf && ./configure-device.sh ) <<EOF
 y
 1837
 n
-1
-0
+${TWOG}
+${FIVG}
 y
 EOF
